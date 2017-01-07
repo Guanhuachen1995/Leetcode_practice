e8e953315703620fba8daaa56eb835c88c1c7018
Gunahuachen1995 <chenguanhua102@163.com>
2017-01-07 22:28:31 +0800

    Create 9.py

---
+++ algorithm/9. Palindrome Number/9.py
@@ -0,0 +1,10 @@
+if x < 0:
+            return False
+        copy, reverse = x, 0
+        
+        while copy:
+            reverse *= 10
+            reverse += copy % 10
+            copy /= 10
+        
+        return x == reverse
