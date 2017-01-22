ccec9a7616d86beecd8baa91921f066928a0cc3b
Gunahuachen1995 <chenguanhua102@163.com>
2017-01-22 16:23:35 +0800

    Create 45.cpp

---
+++ algorithm/45. Jump Game II/45.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int ret = 0;
+        int curMax = 0;
+        int curRch = 0;
+        for(int i = 0; i < nums.size(); i ++)
+        {
+            if(curRch < i)
+            {
+                ret ++;
+                curRch = curMax;
+            }
+            curMax = max(curMax, nums[i]+i);
+        }
+        return ret;
+    }
+};
