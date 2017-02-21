dd508c5fbb41c66503ca9fa6e41326a7e46abafd
Gunahuachen1995 <chenguanhua102@163.com>
2017-02-21 16:05:15 +0800

    Update 96.cpp

--- algorithm/96. Unique Binary Search Trees/96.cpp
+++ algorithm/96. Unique Binary Search Trees/96.cpp
@@ -10,12 +10,17 @@
  * So, the formulation is:
  *      F(n) = F(0) * F(n-1) + F(1) * F(n-2) + F(2) * F(n-3) + ... + F(n-2) * F(1) + F(n-1) * F(0)
  */
-int dp[n+1];
-    dp[0] = dp[1] = 1;
-    for (int i=2; i<=n; i++) {
-        dp[i] = 0;
-        for (int j=1; j<=i; j++) {
-            dp[i] += dp[j-1] * dp[i-j];
+class Solution {
+public:
+    int numTrees(int n) {
+        int dp[n+1];
+        dp[0] = dp[1] = 1;
+        for (int i=2; i<=n; i++) {
+            dp[i] = 0;
+            for (int j=1; j<=i; j++) {
+                dp[i] += dp[j-1] * dp[i-j];
+            }
         }
+        return dp[n];
     }
-    return dp[n];
+};
