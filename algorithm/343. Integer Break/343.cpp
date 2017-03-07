82aa1be5c6b14ac7aef43ab80fb17fac68f7f4be
Gunahuachen1995 <chenguanhua102@163.com>
2017-03-07 09:54:41 +0800

    Create 343.cpp

---
+++ algorithm/343. Integer Break/343.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n<4) return n-1;
+        int result=1;
+        while(n>4)
+        {
+            result*=3;
+            n-=3;
+        }
+        result*=n;
+        return result;
+    }
+};
