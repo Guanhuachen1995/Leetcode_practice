f265944aac8695ade73a823d4f84f8e05fbe8bfe
Gunahuachen1995 <chenguanhua102@163.com>
2017-03-07 11:10:50 +0800

    Create 167.py

---
+++ algorithm/167. Two Sum II - Input array is sorted/167.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        low,high=0, len(numbers)-1
+        while numbers[low]+numbers[high] != target:
+            if numbers[low] + numbers[high] > target:
+                high-=1
+            else:
+                low +=1
+        return [low+1,high+1]
