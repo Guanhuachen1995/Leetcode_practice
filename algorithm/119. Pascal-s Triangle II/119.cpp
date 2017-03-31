8a9af67dc17f932127b801ade41bc708cfb77386
Gunahuachen1995 <chenguanhua102@163.com>
2017-03-31 11:16:59 +0800

    Update 119.cpp

--- algorithm/119. Pascal-s Triangle II/119.cpp
+++ algorithm/119. Pascal-s Triangle II/119.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> r;
-        r.resize(rowIndex + 1);
-        r[0] = r[rowIndex] = 1;
-        for(auto i = 1; i < (r.size() + 1)/2; ++i)
-        {
-            r[i] = r[rowIndex - i] = (unsigned long)r[i - 1]*(unsigned long)(rowIndex - i + 1)/i;
-        }
-        return r;
+        vector<int> A(rowIndex+1, 0);
+        A[0] = 1;
+        for(int i=1; i<rowIndex+1; i++)
+            for(int j=i; j>=1; j--)
+                A[j] += A[j-1];
+        return A;
     }
 };
