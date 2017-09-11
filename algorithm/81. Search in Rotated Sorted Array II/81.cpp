77a3921cdeab2b3398b758e021e0760e531c5fcf
Gunahuachen1995 <chenguanhua102@163.com>
2017-09-10 23:17:24 -0400

    night work

--- algorithm/81. Search in Rotated Sorted Array II/81.cpp
+++ algorithm/81. Search in Rotated Sorted Array II/81.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    bool search(vector<int>& nums, int target) {
         int start = 0, end = nums.size() - 1;
-        if(end == -1) return -1;
+        if(end == -1) return 0;
         while(start < end)
         {
             int mid = (start + end) / 2;
             if(nums[mid] == target)
-                return mid;
+                return 1;
             if(nums[mid] > nums[end])
             {
                 if(nums[mid] > target && nums[start] <= target) end = mid;
                 else
                     start = mid + 1;
             }
-            else
+            else if(nums[mid] < nums[end])
             {
                 if(nums[mid] < target && nums[end] >= target) start = mid + 1;
                 else
                     end = mid;
             }
+            else
+                end--;
         }
-        return nums[start] == target?start:-1;
+        return nums[start] == target?1:0;
     }
 };
