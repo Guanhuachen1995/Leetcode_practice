96ea5f0b88755c10752cbf11f091c5fb5d209897
Gunahuachen1995 <chenguanhua102@163.com>
2017-10-10 22:32:19 -0400

    Create 34_2.cpp

---
+++ algorithm/34. Search for a Range/34_2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1;
+        vector<int> res(2, -1);
+        if(nums.size() == 0)    return res;
+        while(start < end)
+        {
+            int mid = (start + end)/2;
+            if(nums[mid] < target)
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        if(nums[start] != target)   return res;
+        res[0] = start;
+        
+        end = nums.size() - 1;
+        while(start < end)
+        {
+            int mid = (start + end)/2 + 1;
+            if(nums[mid] > target)
+                end = mid - 1;
+            else
+                start = mid;
+        }
+        res[1] = end;
+        return res;
+    }
+};
