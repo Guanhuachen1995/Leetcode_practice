6d83717b0a702192a868f2e37bc232ef5e0c2c07
Gunahuachen1995 <chenguanhua102@gmail.com>
2018-01-24 15:20:00 -0500

    Create 207.cpp

---
+++ algorithm/207. Course Schedule/207.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<int> res;
+        vector<int> indeg(numCourses,0);
+        for(auto pre:prerequisites)
+        {
+            indeg[pre.first]++;
+        }
+        queue<int> q;
+        for(int i = 0; i < indeg.size(); i++)
+        {
+            if(indeg[i] == 0)   
+            {
+                q.push(i);
+                indeg[i]--;
+            }
+        }
+        
+        
+        while(!q.empty())
+        {
+            int v = q.front();
+            q.pop();
+            res.push_back(v);
+            for(auto pre:prerequisites)
+            {
+                if(pre.second == v)
+                    indeg[pre.first]--;
+            }
+            
+            for(int i = 0; i < indeg.size(); i++)
+            {
+                if(indeg[i] == 0)
+                {
+                q.push(i);
+                indeg[i]--;
+                }
+            }
+        }
+        if(res.size()<numCourses) return 0;
+        return 1;
+    }
+};
