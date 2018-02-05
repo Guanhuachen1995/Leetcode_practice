ff195879a1eac2d0452e4216a96dfbddbea97d3d
Guanhuachen1995 <chenguanhua102@gmail.com>
2018-02-05 17:13:11 -0500

    Create 251.cpp

---
+++ algorithm/251. Flatten 2D Vector/251.cpp
@@ -0,0 +1,26 @@
+class Vector2D {
+    vector<vector<int>>::iterator i, iEnd;
+    int j = 0;
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+        i = vec2d.begin();
+        iEnd = vec2d.end();
+    }
+
+    int next() {
+        hasNext();
+        return (*i)[j++];
+    }
+
+    bool hasNext() {
+        while (i != iEnd && j == (*i).size())
+            i++, j = 0;
+        return i != iEnd;
+    }
+};
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i(vec2d);
+ * while (i.hasNext()) cout << i.next();
+ */
