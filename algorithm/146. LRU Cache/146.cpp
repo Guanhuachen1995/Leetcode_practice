1518af64062feb9e9c8baaf88c836cea81b95109
Guanhuachen1995 <chenguanhua102@gmail.com>
2018-03-31 16:35:11 -0400

    Create 146.cpp

---
+++ algorithm/146. LRU Cache/146.cpp
@@ -0,0 +1,45 @@
+class LRUCache {
+    int capacity;
+    list<int> recent;
+    unordered_map<int, int> cache;
+    unordered_map<int, list<int>::iterator> pos;
+    
+    void use(int key) {
+        if(pos.find(key) != pos.end())
+            recent.erase(pos[key]);
+        else if(recent.size() >= capacity)
+        {
+            int old = recent.back();
+            recent.pop_back();
+            pos.erase(old);
+            cache.erase(old);
+        }
+        recent.push_front(key);
+        pos[key] = recent.begin();
+    }
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if(cache.find(key) != cache.end())
+        {
+            use(key);
+            return cache[key];
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        use(key);
+        cache[key] = value;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
