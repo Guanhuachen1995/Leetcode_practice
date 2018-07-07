796e7d9b7b27f5e94d0f4b2523f0ee739f071cae
Guanhuachen1995 <chenguanhua102@gmail.com>
2018-07-07 09:52:25 +0800

    Update 15.java

--- algorithm/15. 3Sum/15.java
+++ algorithm/15. 3Sum/15.java
@@ -1,17 +1,25 @@
-Arrays.sort(num);
-        List<List<Integer>> res = new LinkedList<>(); 
-        for (int i = 0; i < num.length-2; i++) {
-            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-                while (lo < hi) {
-                    if (num[lo] + num[hi] == sum) {
-                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
-                        while (lo < hi && num[lo] == num[lo+1]) lo++;
-                        while (lo < hi && num[hi] == num[hi-1]) hi--;
-                        lo++; hi--;
-                    } else if (num[lo] + num[hi] < sum) lo++;
-                    else hi--;
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums.length < 3) return res;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int l = i + 1, r = nums.length - 1;
+            while(l < r) {
+                if(nums[i] + nums[l] + nums[r] == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while(++ l < r && nums[l] == nums[l - 1]); // 跳过重复元素
+                    while(l < -- r && nums[r] == nums[r + 1]); // 跳过重复元素
+                    continue;
                 }
+                if(nums[i] + nums[l] + nums[r] < 0)
+                    l++;
+                else
+                    r--;
             }
         }
-        return res
+        return res;
+    }
+}
