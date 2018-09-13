4f6b0d5348d48c7e984eb00f04f815b63d11010f
Guanhuachen1995 <chenguanhua102@gmail.com>
2018-09-13 15:39:22 -0400

    Update 209.cpp

--- algorithm/209. Minimum Size Subarray Sum/209.cpp
+++ algorithm/209. Minimum Size Subarray Sum/209.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int minSubArrayLen(int s, vector<int>& nums) {
-        int p1 = 0, p2 = 0, min1 = INT_MAX, sum = 0;
-        if (nums.size() == 0)
-            return 0;
+    /**
+     * @param nums: an array of integers
+     * @param s: An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    int minimumSize(vector<int> &nums, int s) {
+        // write your code here
+        int res = INT_MAX;
+        int l = 0, r = 0, sum = 0;
         
-        while(p2 < nums.size())
-        {
-            sum += nums[p2++];
-            while(sum >= s)
-            {
-                min1 = min(min1, p2 - p1 );
-                sum -= nums[p1++];
+        while(r < nums.size()) {
+            while(sum < s && r < nums.size()) {
+                sum += nums[r++];
             }
+            if (sum < s) break;
+            while(sum >= s && l < r) {
+                sum -= nums[l++];
+            }
+            res = min(res, r - l + 1);
         }
-        return min1 == INT_MAX?0:min1;
+        return res == INT_MAX?-1:res;
     }
 };
