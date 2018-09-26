6cc011861fc6bcd68511f6bd02d5bffe59b0b7a4
Guanhuachen1995 <chenguanhua102@gmail.com>
2018-09-26 15:59:53 -0400

    Create 322.cpp

---
+++ algorithm/322. Coin Change/322.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int maxamount = amount +1;
+        vector<int> dp(amount+1, maxamount);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++) {
+            for(int c:coins) {
+                if(c <= i) {
+                    dp[i] = min(dp[i], dp[i - c] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount?-1:dp[amount];
+    }
+};
