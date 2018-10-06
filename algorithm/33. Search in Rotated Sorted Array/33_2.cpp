8539f322a68ce4a0707ccfb17dc165c1605eebc8
Guanhuachen1995 <chenguanhua102@gmail.com>
2018-10-06 19:58:47 -0400

    Update 33_2.cpp

--- algorithm/33. Search in Rotated Sorted Array/33_2.cpp
+++ algorithm/33. Search in Rotated Sorted Array/33_2.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         if(nums.size() == 0)    return -1;
-        int start = 0, end = nums.size() - 1;
-        while(start < end)
-        {
-            int mid = (start + end)/2;
-            if(nums[mid] > nums[end])
-            {
-                if(target > nums[mid] || target <= nums[end])
-                    start = mid + 1;
+        int l = 0, r = nums.size() - 1;
+        while(l + 1 < r) {
+            int mid = l + (r - l)/2;
+            //if(nums[mid] == target) return mid;
+            if(nums[mid] > nums[l]) {
+                if(nums[mid] > target && target >= nums[l])
+                    r = mid;
                 else
-                    end = mid;
-            }
-            else
-            {
-                if(target > nums[mid] && target <= nums[end])
-                    start = mid + 1;
+                    l =mid;
+            } else {
+                if(nums[mid] < target && target <= nums[r])
+                    l = mid;
                 else
-                    end = mid;
+                    r = mid;
             }
         }
-        if(start == end && nums[start] != target)  return -1;
-        return start;
+        if(nums[l] == target)   return l;
+        if(nums[r] == target)   return r;
+        return -1;
     }
 };
