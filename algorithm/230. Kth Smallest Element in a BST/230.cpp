f9ff501f506fac3f3b66dbd0fa5a00e3f870404c
Guanhuachen1995 <chenguanhua102@gmail.com>
2018-10-23 14:29:09 -0400

    Update 230.cpp

--- algorithm/230. Kth Smallest Element in a BST/230.cpp
+++ algorithm/230. Kth Smallest Element in a BST/230.cpp
@@ -9,18 +9,23 @@
  */
 class Solution {
 public:
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> resarr;
-        inorder(root, resarr);
-        return resarr[k-1];
-    }
-    
-    void inorder(TreeNode* root, vector<int>& resarr)
-    {
-        if(root == NULL)
-            return;
-        inorder(root->left, resarr);
-        resarr.push_back(root->val);
-        inorder(root->right, resarr);
+    int kthSmallest(TreeNode* root, int &k) {
+        stack<TreeNode*>    st;
+        while(root) {
+            st.push(root);
+            root = root->left;
+        }
+        
+        while(k > 0) {
+            TreeNode* p = st.top();
+            st.pop();
+            if(--k == 0)    return p->val;
+            p = p->right;
+            while(p) {
+                st.push(p);
+                p = p->left;
+            }
+        }
+        return -1;
     }
 };
